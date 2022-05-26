914b16c8c2c0fee21fd8223f7e7f07cbcd6a5d54
sentimental <sentimental1243@gmail.com>
2022-05-26 13:44:26 +0300

    batch update

--- PeopleApp/src/main/java/app/dao/PersonDAO.java
+++ PeopleApp/src/main/java/app/dao/PersonDAO.java
@@ -2,11 +2,15 @@ package app.dao;
 
 import app.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -41,5 +45,38 @@ public class PersonDAO {
 
     public void delete(int id) {
         jdbcTemplate.update("DELETE FROM person WHERE id=?", id);
+    }
+
+    /*
+    TEST BATCH UPDATE
+     */
+    public void testBatchUpdate() {
+        List<Person> people = createManyPeople(1000);
+
+        jdbcTemplate.batchUpdate("INSERT INTO person VALUES(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, people.get(i).getId());
+                        ps.setString(2, people.get(i).getName());
+                        ps.setInt(3, people.get(i).getAge());
+                        ps.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+    }
+
+    private List<Person> createManyPeople(int n) {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "@gmail.com"));
+        }
+
+        return people;
     }
 }
