671e11d6be71bb620c9ae1f7d05523ad94b8986a
sentimental <sentimental1243@gmail.com>
2022-06-02 16:30:51 +0300

    authentification 1

---
+++ FirstSpringSecurityApp/src/main/java/com/example/FirstSpringSecurityApp/models/Person.java
@@ -0,0 +1,71 @@
+package com.example.FirstSpringSecurityApp.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Empty name!")
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "year_of_birth")
+    private String yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+    public Person() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(String yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", yearOfBirth='" + yearOfBirth + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
