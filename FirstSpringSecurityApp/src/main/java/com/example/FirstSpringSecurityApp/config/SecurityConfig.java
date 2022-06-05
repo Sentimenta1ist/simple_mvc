4960789e889f44473742af30042fd3e9673e1ca1
sentimental <sentimental1243@gmail.com>
2022-06-05 16:26:25 +0300

    csrf token validation

--- FirstSpringSecurityApp/src/main/java/com/example/FirstSpringSecurityApp/config/SecurityConfig.java
+++ FirstSpringSecurityApp/src/main/java/com/example/FirstSpringSecurityApp/config/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // configure Spring security
 
-        http.csrf().disable()
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
